2d60833cebeaa1aaf86a3bc24e3b51bd4eed5e5a
jujubc <jubsbcorrea@gmail.com>
2025-11-14 14:35:58 -0300

    Exercício 4 - Kaban Completo

---
+++ app/src/main/java/com/juliana/task/ui/TodoFragment.kt
@@ -0,0 +1,33 @@
+package com.juliana.task.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.juliana.task.R
+import com.juliana.task.databinding.FragmentLoginBinding
+
+
+class TodoFragment : Fragment() {
+
+
+    private var _binding: FragmentLoginBinding? = null
+    private val binding get() = _binding!!
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentLoginBinding.inflate(inflater, container,false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
--- app/src/main/java/com/juliana/task/ui/auth/LoginFragment.kt
+++ app/src/main/java/com/juliana/task/ui/auth/LoginFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.juliana.task.R
 import com.juliana.task.databinding.FragmentLoginBinding
 
@@ -20,6 +21,28 @@ class LoginFragment : Fragment() {
     ): View {
         _binding = FragmentLoginBinding.inflate(inflater, container,false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initListener()
+
+    }
+
+    private fun initListener(){
+        binding.btnRegister.setOnClickListener{
+            findNavController().navigate(R.id.action_loginFragment_to_registerFragment)
+
+        }
+
+        binding.btnRecover.setOnClickListener {
+            findNavController().navigate(R.id.action_loginFragment_to_recoverAccountFragment)
+        }
+
+        binding.buttonLogin.setOnClickListener {
+            findNavController().navigate(R.id.action_global_homeFragment)
+        }
     }
 
     override fun onDestroyView() {
